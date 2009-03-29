93f712ecd1086f74ca9366192d368d9f1026dac6
Volodya Kolesnikov <voloko@gmail.com>
2009-03-29 04:17:27 +0400

    remove unused tests

--- test/sdoc_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class SDocTest < Test::Unit::TestCase
-  def test_should_add_shtml_generator_to_generators_list
-    assert(RDoc::RDoc::GENERATORS.has_key?('shtml'), "Should add shtml generator to list.")
-  end
-end
--- test/test_helper.rb
+++
@@ -1,5 +0,0 @@
-require "test/unit"
-require "rdoc/rdoc"
-require "sdoc"
-
-$:.unshift "#{File.dirname(__FILE__)}/../lib"

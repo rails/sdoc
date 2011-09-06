125827ed06c0e929be45d0cdb30b574050646956
Vladimir Kolesnikov <voloko@gmail.com>
2011-09-05 17:06:32 -0700

    fix merge

--- lib/sdoc/merge.rb
+++ lib/sdoc/merge.rb
@@ -13,24 +13,22 @@ require 'sdoc/templatable'
 
 class SDoc::Merge
   include SDoc::Templatable
-  
+
   FLAG_FILE = "created.rid"
-  
+
   def initialize()
     @names = []
     @urls = []
     @op_dir = 'doc'
     @title = ''
     @directories = []
-    template_dir = RDoc::Generator::SDoc.template_dir_for('merge')
-		@template_dir = Pathname.new File.expand_path(template_dir)
   end
-  
+
   def merge(options)
     parse_options options
-    
+
 		@outputdir = Pathname.new( @op_dir )
-		
+
     check_directories
     setup_output_dir
     setup_names
@@ -40,23 +38,23 @@ class SDoc::Merge
     merge_tree
     generate_index_file
   end
-  
+
   def parse_options(options)
-    opts = OptionParser.new do |opt|     
+    opts = OptionParser.new do |opt|
       opt.banner = "Usage: sdoc-merge [options] directories"
-      
+
       opt.on("-n", "--names [NAMES]", "Names of merged repositories. Comma separated") do |v|
         @names = v.split(',').map{|name| name.strip }
       end
-      
+
       opt.on("-o", "--op [DIRECTORY]", "Set the output directory") do |v|
         @op_dir = v
       end
-      
+
       opt.on("-t", "--title [TITLE]", "Set the title of merged file") do |v|
         @title = v
       end
-      
+
       opt.on("-u", "--urls [URLS]", "Paths to merged docs. If you",
                    "set this files and classes won't be actualy",
                    "copied to merged build") do |v|
@@ -64,15 +62,16 @@ class SDoc::Merge
       end
     end
     opts.parse! options
+    @template_dir = Pathname.new(RDoc::Options.new.template_dir_for 'merge')
     @directories = options.dup
   end
-  
+
   def merge_tree
     tree = []
     @directories.each_with_index do |dir, i|
       name = @names[i]
       url = @urls.empty? ? name : @urls[i]
-      filename = File.join dir, RDoc::Generator::SHtml::TREE_FILE
+      filename = File.join dir, RDoc::Generator::SDoc::TREE_FILE
       data = open(filename).read.sub(/var tree =\s*/, '')
       subtree = JSON.parse(data, :max_nesting => 0)
       item = [
@@ -83,14 +82,14 @@ class SDoc::Merge
       ]
       tree << item
     end
-    
-    dst = File.join @op_dir, RDoc::Generator::SHtml::TREE_FILE
+
+    dst = File.join @op_dir, RDoc::Generator::SDoc::TREE_FILE
     FileUtils.mkdir_p File.dirname(dst)
     File.open(dst, "w", 0644) do |f|
       f.write('var tree = '); f.write(tree.to_json(:max_nesting => 0))
     end
   end
-  
+
   def append_path subtree, path
     subtree.map do |item|
       item[1] = path + '/' + item[1] unless item[1].empty?
@@ -98,18 +97,18 @@ class SDoc::Merge
       item
     end
   end
-  
+
   def merge_search_index
     items = []
     @indexes = {}
     @directories.each_with_index do |dir, i|
       name = @names[i]
       url = @urls.empty? ? name : @urls[i]
-      filename = File.join dir, RDoc::Generator::SHtml::SEARCH_INDEX_FILE
+      filename = File.join dir, RDoc::Generator::SDoc::SEARCH_INDEX_FILE
       data = open(filename).read.sub(/var search_data =\s*/, '')
       subindex = JSON.parse(data, :max_nesting => 0)
       @indexes[name] = subindex
-      
+
       searchIndex = subindex["index"]["searchIndex"]
       longSearchIndex = subindex["index"]["longSearchIndex"]
       subindex["index"]["info"].each_with_index do |info, j|
@@ -126,7 +125,7 @@ class SDoc::Merge
       # type (class/method/file) or name or doc part or namespace
       [a[:info][5], a[:info][0], a[:info][6], a[:info][1]] <=> [b[:info][5], b[:info][0], b[:info][6], b[:info][1]]
     end
-    
+
     index = {
       :searchIndex => items.map{|item| item[:searchIndex]},
       :longSearchIndex => items.map{|item| item[:longSearchIndex]},
@@ -136,14 +135,14 @@ class SDoc::Merge
       :index => index,
       :badges => @names
     }
-    
-    dst = File.join @op_dir, RDoc::Generator::SHtml::SEARCH_INDEX_FILE
+
+    dst = File.join @op_dir, RDoc::Generator::SDoc::SEARCH_INDEX_FILE
     FileUtils.mkdir_p File.dirname(dst)
     File.open(dst, "w", 0644) do |f|
       f.write('var search_data = '); f.write(search_data.to_json(:max_nesting => 0))
     end
   end
-  
+
   def extract_index_path dir
     filename = File.join dir, 'index.html'
     content = File.open(filename) { |f| f.read }
@@ -154,16 +153,16 @@ class SDoc::Merge
       ''
     end
   end
-  
+
   def generate_index_file
     templatefile = @template_dir + 'index.rhtml'
     outfile      = @outputdir + 'index.html'
     url          = @urls.empty? ? @names[0] : @urls[0]
     index_path   = url + '/' + extract_index_path(@directories[0])
-	  
+
     render_template templatefile, binding(), outfile
   end
-  
+
   def setup_names
     unless @names.size > 0
       @directories.each do |dir|
@@ -173,13 +172,13 @@ class SDoc::Merge
       end
     end
   end
-  
+
   def copy_docs
     @directories.each_with_index do |dir, i|
       name = @names[i]
-      index_dir = File.dirname(RDoc::Generator::SHtml::TREE_FILE)
+      index_dir = File.dirname(RDoc::Generator::SDoc::TREE_FILE)
       FileUtils.mkdir_p(File.join(@op_dir, name))
-      
+
       Dir.new(dir).each do |item|
         if File.directory?(File.join(dir, item)) && item != '.' && item != '..' && item != index_dir
           FileUtils.cp_r File.join(dir, item), File.join(@op_dir, name, item), :preserve => true
@@ -187,38 +186,38 @@ class SDoc::Merge
       end
     end
   end
-  
+
   def copy_files
     dir = @directories.first
     Dir.new(dir).each do |item|
-      if item != '.' && item != '..' && item != RDoc::Generator::SHtml::FILE_DIR && item != RDoc::Generator::SHtml::CLASS_DIR
+      if item != '.' && item != '..' && item != RDoc::Generator::SDoc::FILE_DIR && item != RDoc::Generator::SDoc::CLASS_DIR
         FileUtils.cp_r File.join(dir, item), @op_dir, :preserve => true
       end
     end
   end
-  
+
   def setup_output_dir
     if File.exists? @op_dir
       error "#{@op_dir} allready exists"
     end
     FileUtils.mkdir_p @op_dir
   end
-  
+
   def check_directories
     @directories.each do |dir|
-      unless File.exists?(File.join(dir, FLAG_FILE)) && 
-      File.exists?(File.join(dir, RDoc::Generator::SHtml::TREE_FILE)) && 
-      File.exists?(File.join(dir, RDoc::Generator::SHtml::SEARCH_INDEX_FILE))
+      unless File.exists?(File.join(dir, FLAG_FILE)) &&
+      File.exists?(File.join(dir, RDoc::Generator::SDoc::TREE_FILE)) &&
+      File.exists?(File.join(dir, RDoc::Generator::SDoc::SEARCH_INDEX_FILE))
         error "#{dir} does not seem to be an sdoc directory"
       end
     end
   end
-  
+
   ##
   # Report an error message and exit
 
   def error(msg)
     raise RDoc::Error, msg
   end
-  
+
 end

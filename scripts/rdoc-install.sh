356a17e65e8574ade32c458d944b18e21fd014bd
Zachary Scott <mail@zzak.io>
2016-07-10 12:28:10 +0900

    Move rake and hoe installation, with other rdoc dependencies to script

--- scripts/rdoc-install.sh
+++ scripts/rdoc-install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -ev
 if [ "${rdoc}" = "master" ]; then
+  echo "Installing RDoc HEAD..."
+  gem install rake -v"< 11"
+  gem install hoe
+  gem install kpeg -v"~> 0.9"
+  gem install racc -v"~> 1.4"
+  gem install minitest -v"~> 4.7"
   git clone --depth=1 https://github.com/rdoc/rdoc
   cd rdoc
   rake

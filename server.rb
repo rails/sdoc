421719c4947dcd0d3a5536d0d117ea5a4f8c97c9
localhostdotdev <root@localhost.dev>
2019-03-23 11:37:24 +0100

    simplify server with find

--- server.rb
+++ server.rb
@@ -1,40 +1,33 @@
 require 'sinatra'
+require 'find'
 
-set :public_folder, "doc/"
+set :public_folder, "doc"
 set :port, 8000
 
 def h(html)
   Rack::Utils.escape_html(html)
 end
 
-def link(file, parent:)
-  "<a href='/#{h(file.sub(settings.public_folder, ''))}'>#{h(file.sub(parent, ''))}</a>"
+def relative(path)
+  path.sub(settings.public_folder, '')
 end
 
-def li(file, parent: "")
-  if Dir.exists?(file)
-    file = "#{file}/"
-    "<li>#{link(file, parent: parent)}#{ul(file)}</li>"
-  else
-    "<li>#{link(file, parent: parent)}</li>"
-  end
-end
-
-def ul(dir)
-  str = "<ul>"
-
-  Dir[dir + "*"].sort.map do |file|
-    str += li(file, parent: dir)
-  end
-
-  str + "</ul>"
+def li(path)
+  "<li><a href=\"/#{h(relative(path))}\">#{h(relative(path))}</a></li>"
 end
 
 get '/' do
-  send_file File.join(settings.public_folder, 'index.html')
+  send_file File.join(settings.public_folder, '/index.html')
 end
 
 not_found do
   status 404
-  ul(settings.public_folder)
+
+  str = "<ul>"
+
+  Find.find(settings.public_folder + '/') do |path|
+    str += li(path)
+  end
+
+  str + "</ul>"
 end

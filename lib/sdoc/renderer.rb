3f81fde9fb1623ae70967812641b480ed917b42a
Jonathan Hefner <jonathan@hefner.pro>
2023-09-15 21:33:43 -0500

    Add SDoc::Renderer#inline
    `SDoc::Renderer#inline` renders a template to the current output buffer
    instead of as a separate string.  It also allows a block to be specified
    so that the template can `yield` to perform interleaved rendering.  For
    example:
    
      ```erb
      <% inline "_partial.erb" do %>
        content
      <% end %>
      ```
    
      ```erb
      <%# _partial.erb %>
      <div><% yield %></div>
      ```
    
    Renders:
    
      ```html
      <div>
        content
      </div>
      ```

--- lib/sdoc/renderer.rb
+++ lib/sdoc/renderer.rb
@@ -7,18 +7,29 @@ class SDoc::Renderer
   def self.compile_erb(path)
     @compiled_erb ||= {}
     @compiled_erb[path] ||= begin
-      erb = ERB.new(File.read(path), trim_mode: "<>")
+      erb = ERB.new(File.read(path), trim_mode: "<>", eoutvar: "self.buffer")
       erb.filename = path
       erb
     end
   end
 
+  attr_reader :buffer
+  def buffer=(*); end # Ignore buffer reset from ERB itself.
+
   def initialize(context, rdoc_options)
     @context = context
     @options = rdoc_options
+    @buffer = nil
   end
 
-  def render(template_path, local_assigns = {})
+  def render(...)
+    original_buffer, @buffer = @buffer, +""
+    inline(...)
+  ensure
+    @buffer = original_buffer
+  end
+
+  def inline(template_path, local_assigns = {})
     template_path = File.expand_path(template_path, @options.template_dir)
     _binding = binding
     local_assigns.each { |name, value| _binding.local_variable_set(name, value) }
